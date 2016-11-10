a0da1e3f6d8439c62f81f4967e1f6cdd1cf24592
Julian Andres Guarin <julian.guarin@fitech.com.co>
2016-11-10 10:04:11 -0500

    Users has the submodule JSON

---
+++ Users.cpp
@@ -0,0 +1,12 @@
+/*
+ *  Users.cpp
+ *  Users
+ *
+ *  Created by Julian Andres Guarin on 11/10/16.
+ *  Copyright © 2016 Julian Andres Guarin. All rights reserved.
+ *
+ */
+
+#include <iostream>
+#include "Users.hpp"
+
---
+++ Users.hpp
@@ -0,0 +1,22 @@
+/*
+ *  Users.hpp
+ *  Users
+ *
+ *  Created by Julian Andres Guarin on 11/10/16.
+ *  Copyright © 2016 Julian Andres Guarin. All rights reserved.
+ *
+ */
+
+#ifndef Users_
+#define Users_
+
+
+class Users
+{
+    public:
+    
+    Users (const char *);
+    
+};
+
+#endif
